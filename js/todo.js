8a28cd00bd937b2a4ef5a6caa50263764ac5e162
jueon <wndjs803@naver.com>
2023-01-24 16:35:36 +0900

    Add a deleteToDo

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,20 @@ const todoForm = document.getElementById("todo-form");
 const todoInput = todoForm.querySelector("input");
 const todoList = document.getElementById("todo-list");
 
+function deleteToDo(event) {
+  const li = event.target.parentElement;
+  li.remove();
+}
+
 function paintToDo(newTodo) {
   const li = document.createElement("li");
   const span = document.createElement("span");
-  li.appendChild(span);
   span.innerText = newTodo;
+  const button = document.createElement("button");
+  button.innerText = "X";
+  button.addEventListener("click", deleteToDo);
+  li.appendChild(span);
+  li.appendChild(button);
   todoList.appendChild(li);
 }
 
