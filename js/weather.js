5615330056425af39dab8ae4d3194084d0f03092
jueon <wndjs803@naver.com>
2023-01-26 17:53:48 +0900

    Complete weater

---
+++ js/weather.js
@@ -0,0 +1,20 @@
+const API_KEY = "f3a72b9c424f08a21aa852842a9bd9d2";
+
+function onGoOk(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = data.weather[0].main;
+    });
+}
+function onGoError() {
+  alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGoOk, onGoError);
