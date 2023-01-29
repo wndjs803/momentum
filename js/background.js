85fabc6b76d2f10325b2c974084d4ac4ef0fd897
jueon <wndjs803@naver.com>
2023-01-29 14:51:19 +0900

    Update bookmark and add a font

--- js/background.js
+++ js/background.js
@@ -5,5 +5,5 @@ const chosenImage = images[Math.floor(Math.random() * images.length)];
 const bgImage = document.createElement("img");
 
 bgImage.src = `img/${chosenImage}`;
-
+bgImage.id = "bg";
 document.body.appendChild(bgImage);
