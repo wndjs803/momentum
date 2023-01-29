e5e937db7efe4c61d8a1ff98e78bdb72568b428b
jueon <wndjs803@naver.com>
2023-01-29 14:05:50 +0900

    Update icon

--- js/clock.js
+++ js/clock.js
@@ -1,4 +1,4 @@
-const clock = document.querySelector("h2#clock");
+const clock = document.querySelector("header #clock");
 
 function getClock() {
   const data = new Date();
--- js/weather.js
+++ js/weather.js
@@ -7,7 +7,7 @@ function onGoOk(position) {
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
-      const weather = document.querySelector("#weather span:first-child");
+      const weather = document.querySelector("#weather span:nth-child(2)");
       const city = document.querySelector("#weather span:last-child");
       city.innerText = data.name;
       weather.innerText = data.weather[0].main;
